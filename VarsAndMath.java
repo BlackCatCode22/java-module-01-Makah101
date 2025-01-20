c0acc97ec66e5f67ba8784b9fbc943d9b317833b
Marco Jimenez <0675654@my.scccd.edu>
2025-01-20 00:20:58 -0800

    Added Program 2 VarsAndMath

---
+++ VarsAndMath.java
@@ -0,0 +1,13 @@
+public class VarsAndMath {
+    public static void main(String[] args) {
+        int age = 16;
+        int hfeet = 5;
+        int hinches = 7;
+        String Name = "Sam";
+
+        System.out.println(Name + " is " + age + " years old and is " + hfeet + "'" + hinches + " in height. In five " +
+                "years she will be " + (age + 5) + ".");
+
+
+    }
+}
